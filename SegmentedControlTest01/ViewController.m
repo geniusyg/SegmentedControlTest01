640fa1eba344b8b192d83fd01f2198b385ab4be7
Chae Yungi <l8673901@gmail.com>
2014-01-03 13:58:01 +0900

    첫번째 SementControlTest입니다.
    github에 올리는 첫 프로젝트!!

--- SegmentedControlTest01/ViewController.m
+++ SegmentedControlTest01/ViewController.m
@@ -12,12 +12,53 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+	UISegmentedControl *segment;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	UIImage *img1 = [UIImage imageNamed:@"ball1.png"];
+	UIImage *img2 = [UIImage imageNamed:@"ball2.png"];
+	UIImage *img3 = [UIImage imageNamed:@"ball3.png"];
+	
+//	NSArray *items = @[img1, img2, img3];
+	NSArray *items = [NSArray arrayWithObjects:img1, img2,img3, nil];
+	
+	segment = [[UISegmentedControl alloc] initWithItems:items];
+	segment.frame = CGRectMake(20, 200, 300, 40);
+	
+	[self.view addSubview:segment];
+	
+	segment.selectedSegmentIndex = 1;
+	[segment addTarget:self action:@selector(handle:) forControlEvents:UIControlEventValueChanged];
+	
+}
+
+- (void)handle:(UISegmentedControl *)sender {
+	switch (sender.selectedSegmentIndex) {
+		case 0:
+			NSLog(@"이미지1");
+			break;
+		case 1:
+			NSLog(@"이미지2");
+			break;
+		case 2:
+			NSLog(@"이미지3");
+			break;
+			
+		default:
+			break;
+	}
+}
+
+- (IBAction)chooseNumber:(id)sender {
+	UISegmentedControl *control = (UISegmentedControl *)sender;
+	NSString *str = [control titleForSegmentAtIndex:control.selectedSegmentIndex];
+	NSLog(@"Index : %d - Title : %@",control.selectedSegmentIndex, str);
 }
 
 - (void)didReceiveMemoryWarning
